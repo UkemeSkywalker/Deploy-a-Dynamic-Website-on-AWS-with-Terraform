cf04805685f817bde9425233acd9506e66c1c101
UkemeSkywalker <skywalkershadow9@gmail.com>
2023-03-18 23:18:13 +0100

    Store terraform state file in s3 bucket

--- main.tf
+++ main.tf
@@ -1,4 +1,16 @@
+# configure aws provider
 provider "aws" {
   region = "us-east-1"
   profile = "cyrax"
+}
+
+
+# Store terraform state file in s3 bucket
+terraform {
+  backend "s3" {
+    bucket = "cyrax-terraform-remote-state2"
+    key    = "terraform.tfstate.dev"
+    region = "us-east-1"
+    profile = "cyrax"
+  }
 }
