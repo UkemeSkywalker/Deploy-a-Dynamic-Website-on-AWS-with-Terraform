6235453b2b91923e72fa6833b50915e03f178c8f
UkemeSkywalker <skywalkershadow9@gmail.com>
2023-03-21 13:06:10 +0100

    alb and ssh security groups

--- security-group.tf
+++ security-group.tf
@@ -1,50 +1,50 @@
 # create security group for the application load balancer
 # terraform aws create security group
 resource "aws_security_group" "alb_security_group" {
-  name        = 
+  name        = "alb security group"
   description = "enable http/https access on port 80/443"
-  vpc_id      = 
+  vpc_id      = aws_vpc.vpc.id
 
   ingress {
     description      = "http access"
-    from_port        = 
-    to_port          = 
-    protocol         = 
-    cidr_blocks      = 
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 
   ingress {
     description      = "https access"
-    from_port        = 
-    to_port          = 
-    protocol         = 
-    cidr_blocks      = 
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port        = 
-    to_port          = 
-    protocol         = 
-    cidr_blocks      = 
+    from_port        = 0
+    to_port          = 0
+    protocol         = -1
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 
   tags   = {
-    Name = 
+    Name = "alb security group"
   }
 }
 
 # create security group for the bastion host aka jump box
 # terraform aws create security group
 resource "aws_security_group" "ssh_security_group" {
-  name        = 
+  name        = "ssh security group"
   description = "enable ssh access on port 22"
-  vpc_id      = 
+  vpc_id      = aws_vpc.vpc.id
 
   ingress {
     description      = "ssh access"
-    from_port        = 
-    to_port          = 
-    protocol         = 
+    from_port        = 22
+    to_port          = 22
+    protocol         = "ssh"
     cidr_blocks      = 
   }
 
