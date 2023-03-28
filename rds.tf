8693c8512199f768eb670ff69bb3791e7c572595
UkemeSkywalker <skywalkershadow9@gmail.com>
2023-03-28 22:46:03 +0100

    create mysql rds instance

--- rds.tf
+++ rds.tf
@@ -1,28 +1,35 @@
+# use data source to get all avalablility zones in region
+data "aws_availability_zones" "available_zones" {}
+
 # create the subnet group for the rds instance
+
 resource "aws_db_subnet_group" "database_subnet_group" {
-  name         = "database subnet group"
-  subnet_ids   = [var.private_data_subnet_az1_cidr, var.private_data_subnet_az2_cidr]
-  description  = "database subnet group"
+  name         = "database-subnet"
+  subnet_ids   = [aws_subnet.private_data_subnet_az1.id, aws_subnet.private_data_subnet_az2.id ]
+  description  = "subnets for database instance"
 
   tags   = {
-    Name = "database subnet group"
+    Name = "database-subnet"
   }
 }
 
 
 # create the rds instance
-resource "aws_db_instance" "db_instance" {
-  engine                  = 
-  engine_version          = 
-  multi_az                = 
-  identifier              = 
-  username                = 
-  password                = 
-  instance_class          = 
-  allocated_storage       = 
-  db_subnet_group_name    = 
-  vpc_security_group_ids  = 
-  availability_zone       = 
-  db_name                 = 
-  skip_final_snapshot     = 
+
+resource "aws_db_instance" "mysql_db" {
+  engine               = "mysql"
+  engine_version       = "8.0.31"
+  multi_az             = false
+  identifier           = "dev-rds-instance"
+  instance_class       = "db.t2.micro"
+  username             = "skywalker"
+  password             = "examplepassword"
+  allocated_storage    = 200
+  # parameter_group_name = "default.mysql8.0.31"
+  db_subnet_group_name    = aws_db_subnet_group.database_subnet_group.id
+  vpc_security_group_ids  = [aws_security_group.database_security_group.id]
+  availability_zone       = data.aws_availability_zones.available_zones.names[0]
+  db_name = "applicationdb"
+  skip_final_snapshot  = true
+
 }
